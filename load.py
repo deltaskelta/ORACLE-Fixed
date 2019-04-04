corrected cost function gradients

load.py
@@ -16,7 +16,8 @@ def load_data():
 
     # images are stored in this dimension
     image_size = 28
-    num_images = 6000
+    num_images = 30000
+    train_set = 24000
 
     img_f.read(16)  # skip non image data
     label_f.read(8)  # skip non label data
@@ -47,18 +48,14 @@ def load_data():
         i += 1
 
     data = {i: [] for i in range(10)}
+    val = {i: [] for i in range(10)}
+
     for i in range(num_images):
         # since the labels are all scalars, we can just get the 0 index
         digit = label_data[i][0]
-        data[digit].append(img_data[i])
+        if i < train_set:
+            data[digit].append(img_data[i])
+        else:
+            val[digit].append(img_data[i])
 
-    see = input("see sorted examples (y/n): ")  # sanity check
-    if see == 'y':
-        for i in data.keys():
-            print("there are {} examples of {}'s".format(len(data[i]), i))
-            for j in range(5):
-                print("image is a {}".format(i))
-                image = np.asarray(data[i][j]).squeeze()
-                print(image)
-
-    return data
+    return data, val

oracle.py
@@ -2,123 +2,171 @@
 This is an implementation of the ORACLE-Fixed learning algorithm
 """
 
-from matplotlib import pyplot as plt
+import scipy.optimize as op
+#from matplotlib import pyplot as plt
 import numpy as np
-
 from load import load_data
-
 import utils
 
 
+def validation_predict(val_set, thetas, i):
+    """cross validation set prediction"""
+    # predict everything until i to see how it does on those examples
+    print("")
+    for j in range(i + 1):
+        A, _ = utils.predict(utils.add_bias_col(val_set[j]), thetas)
+        predicted = np.argmax(A[1], axis=1)
+        accuracy = np.mean(np.int8(predicted == j)) * 100
+        print("for task {}, accuracy: {} for digit {}".format(i, accuracy, j))
+
+
 # pylint: disable=too-many-locals
-def plain_nn(data, order):
+def plain_nn(data, order, theta_vec, val):
     """
     train a neural network with l2 theta regularization that forgets everything when
     learning a new task
     """
     lambd = 1
-    alpha = 0.3
 
-    thetas = [
-        utils.generate_random_theta(784 + 1, 32 + 1, 32, 785),
-        utils.generate_random_theta(32 + 1, 10, 10, 32 + 1)
-    ]
-
-    x = list()
-    plt.ion()
     for i, _ in enumerate(data, start=0):
-        _, ax = plt.subplots()
-        for j in range(25):
-            y = order[i]
-            m = data[y].shape[0]
+        y = order[i]
+        m = data[y].shape[0]
 
-            complx = utils.l2_normalization(thetas, lambd, m)
-            J, gradients = utils.cost(m, lambd, thetas, y, data[i], complx)
+        res = op.minimize(
+            utils.cost,
+            theta_vec,
+            method='CG',
+            jac=True,
+            options=dict({
+                "disp": True,
+            }),
+            args=(m, lambd, y, data[y]))
 
-            x.append(J)
-            ax.plot(x)
-            plt.pause(0.00001)
-            plt.draw()
-
-            for k, _ in enumerate(thetas):
-                thetas[k] = thetas[k][:, 1:]
-                thetas[k] -= gradients[k] * alpha
-                thetas[k] = utils.add_bias_col(thetas[k])
-
-        plt.clf()
-        x = list()
-
-        # predict everything until i to see how it does on those examples
-        for j in range(i + 1):
-            A, _ = utils.predict(data[j], thetas)
-            predicted = np.argmax(A[1], axis=1)
-            accuracy = np.mean(np.int8(predicted == j)) * 100
-            print("for task {}, standard NN accuracy: {} for digit {}".format(
-                i, accuracy, j))
+        thetas = utils.unravel_theta(res["x"])
+        validation_predict(val, thetas, i)
 
 
-def theta_distance_reg(data, order):
+# pylint: disable=too-many-locals
+def theta_distance_reg(data, order, theta_vec, val):
     """
     train a NN with l2 regularization on thetas^l - thetas^l-1, which should place a higher cost
-    when theta changes a lost from the previous theta
+    when theta changes a more from the previous theta
     """
+    lambd = 1
+    first_theta = np.array([])
+    for i, _ in enumerate(data, start=0):
+        y = order[i]
+        m = data[y].shape[0]
 
-    lambd = 0.3
+        # If we are on the first loop we want regular l2 normalization because we want to
+        # enforce that Thetas should be as low valued as possible. If it is not in loop one
+        # then we can just do theta difference l2 normalization which is the goal
+        if i == 0:
+            res = op.minimize(
+                utils.cost,
+                theta_vec,
+                method='CG',
+                jac=True,
+                options=dict({
+                    "disp": True,
+                }),
+                callback=utils.callback,
+                args=(m, 1, y, data[y]))
+
+            first_theta = res["x"].copy()
+        else:
+            #if i == 1:
+            #    utils.check_td_gradients(theta_vec, y, data[y][:1, :])
+
+            res = op.minimize(
+                utils.theta_diff_cost,
+                theta_vec,
+                method='CG',
+                jac=True,
+                options=dict({
+                    "disp": True,
+                }),
+                callback=utils.callback,
+                args=(first_theta, m, lambd, y, data[y]))
+
+        # set current theta vec for next loop complexity calc
+        theta_vec = res["x"].copy()
+        thetas = utils.unravel_theta(res["x"])
+
+        validation_predict(val, thetas, i)
+
+
+# pylint: disable=too-many-locals
+def oracle_fixed(data, order, theta_vec, val):
+    """
+    train a neural network according to the ORACLE-Fixed algorithm
+    """
+    lambd = 0.1
+    lambd2 = 0.1
     alpha = 0.1
 
-    thetas = [
-        utils.generate_random_theta(784 + 1, 32 + 1, 32, 785),
-        utils.generate_random_theta(32 + 1, 10, 10, 32 + 1)
-    ]
-
-    x = list()
-    plt.ion()
     for i, _ in enumerate(data, start=0):
-        _, ax = plt.subplots()
+        y = order[i]
+        m = data[y].shape[0]
 
-        thetas_prev = [t.copy() for t in thetas]
-        for j in range(15):
-            y = order[i]
-            m = data[y].shape[0]
+        # If we are on the first loop we want regular l2 normalization because we want to
+        # enforce that Thetas should be as low valued as possible. If it is not in loop one
+        # then we can just do theta difference l2 normalization which is the goal
+        if i == 0:
+            res = op.minimize(
+                utils.cost,
+                theta_vec,
+                method='CG',
+                jac=True,
+                options=dict({
+                    "disp": True,
+                }),
+                callback=utils.callback,
+                args=(m, 1, y, data[y]))
+        else:
+            shared_prev = theta_vec.copy()
+            unrolled = utils.unravel_theta(shared_prev)
 
-            # If we are on the first loop we want regular l2 normalization because we want to
-            # enforce that Thetas should be as low valued as possible. If it is not in loop one
-            # then we can just do theta difference l2 normalization which is the goal
-            if i == 0:
-                complx = utils.l2_normalization(
-                    thetas, 1, m)  # has a different lambda value
-            else:
-                complx = utils.theta_difference_l2_normalization(
-                    thetas, thetas_prev, lambd)
-                print("complx is: {}".format(complx))
+            ps = list()
+            qs = list()
+            for j, th in enumerate(unrolled):
+                tau, p, q = utils.make_tau(th)
+                unrolled[j] += tau
+                ps.append(p)
+                qs.append(q)
 
-            J, gradients = utils.cost(m, lambd, thetas, y, data[y], complx)
+            rolled = utils.ravel_theta(unrolled)
 
-            x.append(J)
-            ax.plot(x)
-            plt.pause(0.00001)
-            plt.draw()
+            res = op.minimize(
+                utils.oracle_fixed_cost,
+                rolled,
+                method='CG',
+                jac=True,
+                options=dict({
+                    "disp": True,
+                }),
+                callback=utils.callback,
+                args=(shared_prev, ps, qs, m, lambd, lambd2, y, data[y]))
 
-            for k, _ in enumerate(thetas):
-                thetas[k] = thetas[k][:, 1:]
-                thetas[k] -= gradients[k] * alpha
-                thetas[k] = utils.add_bias_col(thetas[k])
+        theta_vec = res["x"].copy()
 
-        plt.clf()
-        x = list()
-
-        # predict everything until i to see how it does on those examples
-        for j in range(i + 1):
-            A, _ = utils.predict(data[j], thetas)
-            predicted = np.argmax(A[1], axis=1)
-            accuracy = np.mean(np.int8(predicted == j)) * 100
-            print("for task {}, standard NN accuracy: {} for digit {}".format(
-                i, accuracy, j))
+        # set current theta vec for next loop complexity calc
+        thetas = utils.unravel_theta(theta_vec.copy())
+        validation_predict(val, thetas, i)
 
 
 if __name__ == '__main__':
-    _data = load_data()
-    _data = utils.reshape_examples(_data)
+    d, v = load_data()
+    d = utils.reshape_examples(d)
+    v = utils.reshape_examples(v)
 
-    theta_distance_reg(_data, list(_data))
-    plain_nn(_data, list(_data))
+    tv = utils.ravel_theta([
+        utils.generate_random_theta(utils.T1[1] + 1, utils.T1[0] + 1,
+                                    utils.T1[0], utils.T1[1]),
+        utils.generate_random_theta(utils.T2[1] + 1, utils.T2[0] + 1,
+                                    utils.T2[0], utils.T2[1])
+    ])
+
+    theta_distance_reg(d, list(d), tv, v)
+    plain_nn(d, list(d), tv, v)
+    #oracle_fixed(d, list(d), tv, v)

utils.py
@@ -3,8 +3,11 @@ utility functions for the ORACLE-Fixed Neural Network
 """
 
 import math
-import random
 import numpy as np
+
+# both will be +1 column for bias
+T1 = (32, 784 + 1)
+T2 = (10, 32 + 1)
 
 
 def sigmoid(x):
@@ -21,37 +24,33 @@ sigmoid = np.vectorize(sigmoid)  # vectorize the sigmoid for future use
 def predict(inputs, thetas):
     """make a prediction"""
 
-    # add bias column to inputs
-    inputs = add_bias_col(inputs)
-
     # feed forward to layer 2 and apply sigmoid
-    z_values = [np.dot(inputs, thetas[0].T)]
-    a_values = [sigmoid(z_values[0])]
-
+    z_values = [np.dot(inputs, thetas[0].T)]  # 1x3 * (5x3).T = 1x5
+    a_values = [sigmoid(z_values[0])]  # 1x5
     # add bias column to a_two
-    a_values[0] = add_bias_col(a_values[0])
-
+    a_values[0] = add_bias_col(a_values[0])  # 1x6
     # feed forward to layer three
+    # 1x6 * (1x6).T = 1x1
     z_values.append(np.dot(a_values[0], thetas[1].T))
-    a_values.append(sigmoid(z_values[1]))
+    a_values.append(sigmoid(z_values[1]))  #1x1
 
     return a_values, z_values
 
 
 def sigmoid_gradient(Z):
     """calculate the sigmoid gradient of a matrix sgm(z) .* 1 - sgm(z)"""
-    return np.multiply(sigmoid(Z), np.subtract(1, sigmoid(Z)))
+    sgd = sigmoid(Z)
+    return sgd * (1 - sgd)
 
 
 def generate_random_theta(l_size, l_next_size, rows, cols):
     """generate random theta values based on layer and next layer size"""
-    epsilon = math.sqrt(6) / math.sqrt(l_size + l_next_size) * rand_sign()
-    return np.random.uniform(low=-epsilon, high=epsilon, size=(rows, cols))
+    epsilon = math.sqrt(6) / math.sqrt(l_size + l_next_size)
+    arr = np.random.uniform(low=-epsilon, high=epsilon, size=(rows, cols))
 
-
-def rand_sign():
-    """return a -1 or 1 randomly"""
-    return -1 if random.random() < 0.5 else 1
+    # add bias column
+    arr[:, 0] = 1
+    return arr
 
 
 def add_bias_col(arr):
@@ -59,57 +58,51 @@ def add_bias_col(arr):
     return np.insert(arr, [0], [1], axis=1)
 
 
-# pylint: disable=too-many-arguments
-def cost(m, lambd, thetas, y, X, complx):
-    """
-    the standard non ORACLE cost function.
-    since we are training by each label in sets, the y value is just an integer label and not a one
-    vs all vector
-    """
+def make_labels(y, m):
+    """make one vs all labels from a scalar value"""
 
-    A, Z = predict(X, thetas)
-
-    # expand the scalar value into a one vs all output vector
-    labels = np.zeros((1, 10))
+    labels = np.zeros((1, T2[0]))
     labels[0][y] = 1
-    labels = np.repeat(labels, m, axis=0)
 
-    # compute the gradients to return as well
-    d_three = np.subtract(A[1], labels)
+    return np.repeat(labels, m, axis=0)
 
-    d_two = np.dot(thetas[1].T[1:, :], d_three.T)
-    d_two = np.multiply(d_two, sigmoid_gradient(Z[0]).T)
 
-    gradients = []
-    gradients.append(np.dot(d_two, X) / m)  # theta 1 gradient
-    gradients.append(np.dot(d_three.T, A[0])[:, 1:] / m)
+def make_tau(sigma):
+    """make the tau parameter for the ORACLE-Fixed algorithm"""
 
-    # the first term of the cost function -y * log(h(x))
-    J = np.multiply(labels, np.log(A[1]))
-    # the second term of the cost function 1 - y * log(h(x))
-    J += np.multiply(1 - labels, np.log(1 - A[1]))
-    # subtract k - l, then sum both dimensions of the matrix
-    J = -np.sum(J) / m
-    # add lambda regularization (complexity term)
-    J += complx
+    u, s, v = np.linalg.svd(sigma, full_matrices=True)
+    p = np.dot(u, np.sqrt(np.diag(s)))
 
-    for i, v in enumerate(gradients):
-        gradients[i] = np.add(v, lambd / m * v)
+    # it should be the same size as theta (p * q.T) and p and q should be sparse
+    # it should a
+    s = np.diag(s)
+    S = np.append(s, np.zeros((v.shape[0] - u.shape[0], s.shape[0])), axis=0)
 
-    return J, gradients
+    q = np.dot(v, np.sqrt(S))
+
+    return np.dot(p, q.T), p, q
+
+
+def log_likelihood(labels, A, m):
+    """the log likelihood portion of the cost function"""
+    # the first term of the cost function -y * log(h(x)), second term 1 - y * log(h(x))
+    J = -labels * np.log(A[1])
+    J -= (1 - labels) * np.log(1 - A[1])
+    return np.sum(J) / m
 
 
 def l2_normalization(thetas, lambd, m):
     """l2 normalization of theta values"""
 
     r = 0
-    for i, _ in enumerate(thetas):
-        r += np.sum(np.square(thetas[i][:, 1:]))
+    for _, v in enumerate(thetas):
+        r += np.sum(np.square(v[:, 1:]))
     return r * lambd / (2 * m)
 
 
-def theta_difference_l2_normalization(thetas, thetas_prev, lambd):
+def theta_difference_l2_normalization(thetas, thetas_prev, m, lambd):
     """l2 normalization based on the difference in theta values"""
+
     r = 0
     for i, _ in enumerate(thetas):
         t = thetas[i] - thetas_prev[i]
@@ -117,32 +110,211 @@ def theta_difference_l2_normalization(thetas, thetas_prev, lambd):
     return r * lambd
 
 
-def make_tau(sigma):
-    """make the tau parameter for the ORACLE-Fixed algorithm"""
+def cost(thetas, m, lambd, y, X):
+    """
+    the standard non ORACLE cost function.
+    since we are training by each label in sets, the y value is just an integer label and not a one
+    vs all vector
+    """
 
-    print("theta size: ", sigma.shape)
-    u, s, v = np.linalg.svd(sigma, full_matrices=True)
-    print("u size: ", u.shape)
-    print("sigma size: ", s.shape)
-    print("v.shape: ", v.shape)
+    X = add_bias_col(X)  # now 1x3
+    thetas = unravel_theta(thetas)  # [5x3, 1x6]
 
-    p = np.dot(u, np.sqrt(np.diag(s)))
-    print("p size: ", p.shape)
+    # A = [1x6, 1x1] Z = [1x5, 1x1]
+    A, Z = predict(X, thetas)
+    labels = make_labels(y, m)
 
-    # TODO: the zeros term needs to expand theta to be compatible with p
-    # you can check p's size and add whatever is needed
+    # compute the deltas and gradients to return as well
+    d_three = A[1] - labels  # 1x1
+    # (1x6).T * 1x1 = 6x1 --> 5x1 (from indexing)
+    d_two = np.dot(thetas[1].T, d_three.T)[1:, :] * sigmoid_gradient(Z[0]).T
 
-    # it should be the same size as theta (p * q.T) and p and q should be sparse
-    # it should a
-    s = np.diag(s)
-    S = np.append(s, np.zeros((v.shape[0] - u.shape[0], s.shape[0])), axis=0)
-    #S = np.append(s, np.zeros(()))
-    print("S shape: ", S.shape)
-    print(p)
-    q = np.dot(v, np.sqrt(S))
-    print(q)
+    gradients = [
+        np.dot(d_two, X) / m,  # 5x1 * 1x3 = 5x3
+        np.dot(d_three.T, A[0]) / m  # 1x1 * 1x6 = 1x6
+    ]
 
-    return np.dot(p, q.T)
+    J = log_likelihood(labels, A, m)
+    J += l2_normalization(thetas, lambd, m)  # lambda complexity penalty
+
+    for i, _ in enumerate(gradients):
+        gradients[i][:, 1:] += lambd / m * thetas[i][:, 1:]
+
+    return J, ravel_theta(gradients)
+
+
+# pylint: disable=too-many-locals, too-many-arguments
+def theta_diff_cost(thetas, thetas_prev, m, lambd, y, X):
+    """
+    the standard non ORACLE cost function.
+    since we are training by each label in sets, the y value is just an integer label and not a one
+    vs all vector
+    """
+
+    thetas_mag = np.sqrt(np.sum(np.square(thetas)))
+    thetas_pre_mag = np.sqrt(np.sum(np.square(thetas_prev)))
+    print("thetas: ", thetas_mag, " thetas_prev: ", thetas_pre_mag)
+
+    # TODO: should this difference be calculated ignoring the bias theta units? if so, this needs to move to
+    # a calulation that happens after the theta values are unrolled
+    err = np.sqrt(np.sum(np.square(thetas[:, 1:] - thetas_prev[:, 1:])))
+    print("calculated error is: ", err)
+
+    X = add_bias_col(X)
+    thetas = unravel_theta(thetas)
+    thetas_prev = unravel_theta(thetas_prev)
+
+    A, Z = predict(X, thetas)
+    labels = make_labels(y, m)
+
+    # compute the deltas and gradients to return as well
+    d_three = A[1] - labels
+    d_two = np.dot(thetas[1].T, d_three.T)[1:, :] * sigmoid_gradient(Z[0]).T
+
+    gradients = [
+        np.dot(d_two, X) / m,  # theta 1 gradient
+        np.dot(d_three.T, A[0]) / m
+    ]
+
+    # add lambda regularization (complexity term)
+    J = log_likelihood(labels, A, m)
+    diff = theta_difference_l2_normalization(thetas, thetas_prev, m, lambd)
+    J += diff
+    print("cost: {} penalization: {}".format(J, diff))
+
+    for i, _ in enumerate(gradients):
+        gradients[i][:, 1:] += 2 * lambd * thetas[i][:, 1:]
+
+    print(J)
+
+    return J, ravel_theta(gradients)
+
+
+def l1_tau(lambd, p, q):
+    """l1 normalization of tau factors"""
+    return lambd * (np.sum(np.abs(p)) + np.sum(np.abs(q)))
+
+
+# pylint: disable=too-many-locals, too-many-arguments
+def oracle_fixed_cost(thetas, shared, ps, qs, m, lambd, lambd2, y, X):
+    """
+    shared cost for the ORACLE-Fixed shared parameter optimization
+    """
+
+    X = add_bias_col(X)
+    thetas = unravel_theta(thetas)
+    thetas_prev = unravel_theta(shared)
+
+    A, Z = predict(X, thetas)  # make prediction with theta + tau
+    labels = make_labels(y, m)
+
+    # compute the gradients to return as well
+    d_three = np.subtract(A[1], labels)
+
+    d_two = np.dot(thetas[1].T, d_three.T)[1:, :]
+    d_two = np.multiply(d_two, sigmoid_gradient(Z[0]).T)
+
+    gradients = []
+    gradients.append(np.dot(d_two, X) / m)  # theta 1 gradient
+    gradients.append(np.dot(d_three.T, A[0]) / m)
+
+    # add lambda regularization (complexity term)
+    J = log_likelihood(labels, A, m)
+    J += theta_difference_l2_normalization(thetas, thetas_prev, m, lambd)
+    for i, _ in enumerate(ps):
+        J += l1_tau(lambd2, ps[i], qs[i])
+
+    for i, v in enumerate(gradients):
+        gradients[i][:, 1:] = np.add(v[:, 1:], lambd / m * v[:, 1:])
+
+    return J, ravel_theta(gradients)
+
+
+def callback(xk):
+    """callback made on every loop of cost by optimization func"""
+    return True
+
+
+def ravel_theta(thetas):
+    """unroll thetas into a big vector"""
+    r = np.array([])
+    for t in thetas:
+        r = np.append(r, t.ravel())
+    return r
+
+
+def unravel_theta(theta_vec):
+    """rehshape the thetas into matrices"""
+    t1 = T1[0] * T1[1]
+    t2 = T2[0] * T2[1]
+
+    theta1_vals = theta_vec[0:t1]
+    theta2_vals = theta_vec[t1:t1 + t2]
+
+    return [theta1_vals.reshape(T1), theta2_vals.reshape(T2)]
+
+
+def check_nn_gradients(theta_vec, y, data):
+    """check gradient implementation"""
+    epsilon = 0.00001
+    m = data.shape[0]
+
+    gradApprox = theta_vec.copy()  # because it is the same size
+    for i, _ in enumerate(theta_vec):
+        thetaPlus = theta_vec.copy()
+        thetaPlus[i] += epsilon
+        thetaMinus = theta_vec.copy()
+        thetaMinus[i] -= epsilon
+
+        jPlus, _ = cost(thetaPlus, m, 1, y, data)
+        jMinus, _ = cost(thetaMinus, m, 1, y, data)
+
+        gradApprox[i] = (jPlus - jMinus) / (2 * epsilon)
+
+    _, grad = cost(theta_vec, m, 1, y, data)
+
+    l2_g = np.sqrt(np.sum(np.square(grad)))
+    l2ga = np.sqrt(np.sum(np.square(gradApprox)))
+    print("l2g: ", l2_g, " l2ga: ", l2ga)
+
+    err = np.sqrt(np.sum(np.square(grad - gradApprox)))
+    print("calculated error is: ", err)
+
+    for i, _ in enumerate(grad):
+        print("grad: {} approximation: {} diff: {}".format(
+            grad[i], gradApprox[i], grad[i] - gradApprox[i]))
+
+
+def check_td_gradients(theta_vec, y, data):
+    """check gradient implementation"""
+    epsilon = 0.00001
+    m = data.shape[0]
+    lambd = 0.001
+
+    gradApprox = theta_vec.copy()  # because it is the same size
+    for i, _ in enumerate(theta_vec):
+        thetaPlus = theta_vec.copy()
+        thetaPlus[i] += epsilon
+        thetaMinus = theta_vec.copy()
+        thetaMinus[i] -= epsilon
+
+        jPlus, _ = theta_diff_cost(thetaPlus, thetaPlus, m, lambd, y, data)
+        jMinus, _ = theta_diff_cost(thetaMinus, thetaMinus, m, lambd, y, data)
+
+        gradApprox[i] = (jPlus - jMinus) / (2 * epsilon)
+
+    _, grad = theta_diff_cost(theta_vec, theta_vec, m, lambd, y, data)
+
+    l2_g = np.sqrt(np.sum(np.square(grad)))
+    l2ga = np.sqrt(np.sum(np.square(gradApprox)))
+    print("l2g: ", l2_g, " l2ga: ", l2ga)
+
+    err = np.sqrt(np.sum(np.square(grad - gradApprox)))
+    print("calculated error is: ", err)
+
+    for i, _ in enumerate(grad):
+        print("grad: {} approximation: {} diff: {}".format(
+            grad[i], gradApprox[i], grad[i] - gradApprox[i]))
 
 
 def reshape_examples(d):
@@ -157,10 +329,28 @@ def reshape_examples(d):
         for j, v in enumerate(d[i]):
             # unroll the example into 1xN vector
             ex = np.reshape(v, (1, n))
-            new[j] = np.multiply(new[0], ex)
 
-        # make the dict key an MxN matrix of examples
+            if i == 0 and j == 0:
+                new[j] = np.multiply(new[0], ex)
+            else:
+                new[j] = ex
+
+        # make the dict key an MxN matrix of examples, regularize the data to be between 0 and 1
         d[i] = new
         print("the {} examples shape: {}".format(i, d[i].shape))
+
+    # scale the features (x - avg(x) / stdev(x))
+    together = np.array([])
+    for i in d.keys():
+        together = np.append(together, d[i].ravel(), axis=0)
+
+    mean = np.mean(together)
+    stddev = np.std(together)
+    print("max: ", np.max(together))
+    print("min: ", np.min(together))
+    print("mean: ", mean, " stddev: ", stddev)
+
+    for i in d.keys():
+        d[i] = (d[i] - mean) / stddev
 
     return d